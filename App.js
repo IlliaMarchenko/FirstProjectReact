c32135e4710f17fa454fe465526604b7a1881d2e
IlliaMarchenko <106097647+IlliaMarchenko@users.noreply.github.com>
2022-05-23 23:15:04 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center"> 
+       <div className="d-flex align-center">
+          <img width={85} height={65} src="/img/logo.png" />
+          <div className="headerInfo">
+            <h3>NICE BOOKS</h3>
+            <p>лучшие бестселлеры мира</p>
+          </div>
+          
+      </div>
+      
+      
+      <ul className="d-flex">
+        <li className="mr-30">
+        <img width={30} height={30} src="/img/bag.png" />
+        <span>1624 грн.</span>
+        </li>
+        <li>
+          <img width={30} height={30} src="/img/user.png" />
+        </li>
+      </ul>
+      
+        </header>
+        <div className="slider">
+
+        </div> 
+        
+    <div className="content">
+      <h1>Все залупа</h1>
+      .... 
+    </div>
+    </div>
+  );
+}
+export default App;
